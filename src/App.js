81fd4944c4e4d26b9d3f17230f134f34b381d475
Fannytje <103598821+Fannytje@users.noreply.github.com>
2022-06-27 22:22:22 +0200

    Added search bar and new image

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import './App.css';
-import dictionary from "./dictionary-icon.jpg";
+import dictionary from "./dictionary-image.png";
+import Dictionary from './Dictionary';
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <div className='container'>
+      <header className="App-header">
       <img src={dictionary} className="App-logo img-fluid" alt="icon" />
-      Hello
+      </header>
+      <main>
+        <Dictionary /> 
+      </main>
+    <footer className="App-footer"> 
+      <small>Coded by Fanny N</small>
+      </footer>
     </div>
+  </div>
   );
 }
 
