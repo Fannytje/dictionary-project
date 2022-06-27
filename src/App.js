b384bd79acd7df7c17b28963a4fab996ec131000
Fannytje <103598821+Fannytje@users.noreply.github.com>
2022-06-27 21:40:08 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import dictionary from "./dictionary-icon.jpg";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <img src={dictionary} className="App-logo img-fluid" alt="icon" />
+      Hello
+    </div>
+  );
+}
+
+
